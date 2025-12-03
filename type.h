345a437acd35fffc731d8d0c6ea7797a64d36b34
Islamsyed <66957756+Islamsyed@users.noreply.github.com>
2025-12-03 22:20:36 +0530

    Create type.h

---
+++ type.h
@@ -0,0 +1,15 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+typedef unsigned char u8;
+typedef char s8;
+typedef unsigned short int u16;
+typedef short int s16;
+typedef unsigned int u32;
+typedef signed int s32;
+typedef float f32;
+typedef double d64;
+typedef const char cs8;
+typedef const unsigned char cu8;
+
+#endif
